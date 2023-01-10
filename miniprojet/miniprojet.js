d49c12bc2975b6956ff2a7089666170a46b88b24
Dendeey <davidsim1035@gmail.com>
2023-01-10 09:10:46 +0000

    Mini projet

--- miniprojet/miniprojet.js
+++ miniprojet/miniprojet.js
@@ -16,12 +16,13 @@ window.addEventListener("DOMContentLoaded", function(){
     let asideBtn = document.getElementById("side-menu-btn");
     let asideHidden = document.getElementById("aside-hidden");
     let mainSection = document.querySelector("body > main > section");
-    let sectionBorderNone = document.querySelector("body > header > section:first-of-type");
+    let sectionBorderNone = document.querySelector("#header-section");
     asideBtn.addEventListener("click", function()
     {
         asideHidden.classList.toggle("close");
         mainSection.classList.toggle("style");
-        sectionBorderNone.classList.toggle("border-none");
+        sectionBorderNone.classList.toggle("border-right");
+        
     });
     
 });
